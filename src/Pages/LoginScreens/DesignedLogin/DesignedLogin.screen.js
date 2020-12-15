bb41de018cbb1d66a17e7e8d75b969cff2adea79
bessonglawrence <orockbessonglawrence@gmail.com>
2020-12-15 14:28:12 +0100

    Moved styles to style sheet

--- src/Pages/LoginScreens/DesignedLogin/DesignedLogin.screen.js
+++ src/Pages/LoginScreens/DesignedLogin/DesignedLogin.screen.js
@@ -8,7 +8,7 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 const DesignedLogin = () => {
     return(
         <View style={styles.mainContainer}>
-            <Text style={{alignSelf: 'center', fontSize: 27, color: 'white', marginTop: 15}}> Sign In </Text>
+            <Text style={styles.headerText}> Sign In </Text>
             <Icon
                 name="guitar-pick"
                 color="white"
@@ -16,7 +16,7 @@ const DesignedLogin = () => {
                 style={{alignSelf: 'center', marginVertical: 30}} 
             />
             <View style={{ marginTop: 10, marginBottom: 8}}>
-                <View style={{flexDirection: 'row', padding: 8, borderColor: 'white', borderRadius: 30, marginVertical: 10, backgroundColor: 'rgba(238,238,238,0.3)'}}>
+                <View style={styles.inputSections}>
                     <Icon
                         name='account-circle'
                         size={30}
@@ -29,7 +29,7 @@ const DesignedLogin = () => {
                         style={{fontSize: 18}}
                     />
                 </View>
-                <View style={{flexDirection: 'row', padding: 8, borderColor: 'white', borderRadius: 30, marginVertical: 10, backgroundColor: 'rgba(238,238,238,0.3)'}}>
+                <View style={styles.inputSections}>
                     <Icon
                         name='lock'
                         size={30}
@@ -46,17 +46,17 @@ const DesignedLogin = () => {
 
             </View>
 
-            <View style={{flexDirection: 'row', borderWidth: .5, padding: 13, borderColor: 'white', borderRadius: 30, marginTop: 40, alignItems: 'center', justifyContent: 'center'}}>
+            <View style={styles.signInButton}>
                 <TouchableOpacity>
-                    <Text style={{fontSize: 22, color: 'white', fontWeight: 'bold'}}> Sign In </Text>
+                    <Text style={styles.buttonText}> Sign In </Text>
                 </TouchableOpacity>
             </View>
 
-            <Text style={{alignSelf:"center", color: 'white', fontSize: 18, marginVertical: 8}}> Forgot Password?</Text>
+            <Text style={styles.forgotPasswordText}> Forgot Password?</Text>
 
-            <View style={{flexDirection: 'row', borderWidth: .5, padding: 13, borderColor: 'white', borderRadius: 30, marginTop: 4, alignItems: 'center', justifyContent: 'center'}}>
+            <View style={styles.CreateAccountButton}>
                 <TouchableOpacity>
-                    <Text style={{fontSize: 22, color: 'white', fontWeight: 'bold'}}> Create Account </Text>
+                    <Text style={styles.buttonText}> Create Account </Text>
                 </TouchableOpacity>
             </View>
 
--- src/Pages/LoginScreens/DesignedLogin/DesignedLogin.style.js
+++ src/Pages/LoginScreens/DesignedLogin/DesignedLogin.style.js
@@ -3,6 +3,51 @@ const styles = {
         flex: 1,
         backgroundColor: 'grey',
         paddingHorizontal: 20
+    },
+    CreateAccountButton:{
+        flexDirection: 'row', 
+        borderWidth: .5, 
+        padding: 13, 
+        borderColor: 'white', 
+        borderRadius: 30, 
+        marginTop: 4, 
+        alignItems: 'center', 
+        justifyContent: 'center'
+    },
+    buttonText:{
+        fontSize: 22, 
+        color: 'white', 
+        fontWeight: 'bold'
+    },
+    forgotPasswordText:{
+        alignSelf:"center", 
+        color: 'white', 
+        fontSize: 18, 
+        marginVertical: 8
+    },
+    signInButton:{
+        flexDirection: 'row', 
+        borderWidth: .5, 
+        padding: 13, 
+        borderColor: 'white', 
+        borderRadius: 30, 
+        marginTop: 40, 
+        alignItems: 'center', 
+        justifyContent: 'center'
+    },
+    inputSections:{
+        flexDirection: 'row', 
+        padding: 8, 
+        borderColor: 'white', 
+        borderRadius: 30, 
+        marginVertical: 10, 
+        backgroundColor: 'rgba(238,238,238,0.3)'
+    },
+    headerText:{
+        alignSelf: 'center', 
+        fontSize: 27, 
+        color: 'white', 
+        marginTop: 15
     }
 }
 
