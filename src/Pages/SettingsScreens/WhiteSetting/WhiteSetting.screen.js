b9a09690c88e4fb8a1394254effea90239f99f3a
Bessong lawrence <orockbessonglawrence@gmail.com>
2021-06-14 11:11:18 +0100

    figure out and implemented the partitions for the whitesettings screen

--- src/Pages/SettingsScreens/WhiteSetting/WhiteSetting.screen.js
+++ src/Pages/SettingsScreens/WhiteSetting/WhiteSetting.screen.js
@@ -15,30 +15,48 @@ const WhiteSetting = () => {
                 </View>
 
                 <View style={{padding: 24}}>
-                    <View style={{flexDirection: 'row', borderBottomWidth: 0.5, borderColor: 'grey', paddingBottom: 20, marginBottom: 22}}>
+                    <View style={{flexDirection: 'row', borderBottomWidth: 0.2, borderColor: 'grey', paddingBottom: 20, marginBottom: 22}}>
                         <View style={{backgroundColor: 'lightblue', height: 22, width: 22, borderRadius: 30, marginTop: 7}}></View>
-                        <Text style={{fontSize: 20, marginLeft: 25}}>Title</Text>
+                        <Text style={{fontSize: 20, marginLeft: 25, color: 'grey' }}>Title</Text>
                     </View>
 
-                        <View style={{justifyContent: 'space-between', flexDirection: 'row', marginBottom: 15}}>
+                        <View style={{justifyContent: 'space-between', flexDirection: 'row', marginBottom: 15, }}>
                             <View style={{flexDirection: 'row'}}>
                                 <Icon name="clock" size={25} color="grey"/>
                                 <Text style={{fontSize: 18, marginLeft: 25}}>ALL DAY EVENT</Text>
                             </View>
                             <Switch />
                         </View>
-                        <View style={{flexDirection: 'row'}}>
+                        <View style={{flexDirection: 'row', borderBottomWidth: 0.2, borderColor: 'grey', paddingBottom: 10}}>
                             <Icon name="logout" color="grey" size={30}/>
-                            <View style={{ borderBottomWidth: 0.5, borderTopWidth: 0.5, borderColor: 'grey',}}>
-                                <View style={{flexDirection: 'row'}}>
-                                    <View style={{borderRightWidth: 0.5, borderColor: 'grey', padding: 5}}>
-                                        <Text>Date</Text>
-                                        <Text>Sat, January 16</Text>
+                            <View>
+                                <View style={{ borderBottomWidth: 0.2, borderTopWidth: 0.2, borderColor: 'grey', marginLeft: 20, paddingVertical: 3}}>
+                                    <View style={{flexDirection: 'row'}}>
+                                        <View style={{borderRightWidth: 0.5, borderColor: 'grey', padding: 5}}>
+                                            <Text style={{color: 'grey'}}>Date</Text>
+                                            <Text style={{fontSize: 18,}}>Sat, January 16</Text>
+                                        </View>
+                                        <View style={{padding: 5, marginLeft: 5}}>
+                                            <Text style={{color: 'grey'}}>Time</Text>
+                                            <View style={{flexDirection: 'row'}}>
+                                                <Text style={{fontSize: 18,}}>8:00</Text>
+                                                <Text style={{color:'grey', fontSize: 18, marginLeft: 5}}>PM</Text>
+                                            </View>
+                                        </View>
                                     </View>
-                                    <View style={{padding: 5}}>
-                                        <Text>Date</Text>
-                                        <Text>Sat, January 16</Text>
-                                    </View>
+                                </View>
+                                <View style={{flexDirection: 'row', marginLeft: 20, paddingTop: 3}}>
+                                        <View style={{borderRightWidth: 0.2, borderColor: 'grey', padding: 5}}>
+                                            <Text style={{color: 'grey'}}>Date</Text>
+                                            <Text style={{fontSize: 18,}}>Sat, January 16</Text>
+                                        </View>
+                                        <View style={{padding: 5, marginLeft: 5}}>
+                                            <Text style={{color: 'grey'}}>Time</Text>
+                                            <View style={{flexDirection: 'row'}}>
+                                                <Text style={{fontSize: 18,}}>8:00</Text>
+                                                <Text style={{color:'grey', fontSize: 18, marginLeft: 5}}>PM</Text>
+                                            </View>
+                                        </View>
                                 </View>
                             </View>
                         </View>
